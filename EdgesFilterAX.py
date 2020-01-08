febd97472db876a6ad2d4eb5daac7549108a525a
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2020-01-08 10:15:16 +0100

    EdgesFilterAX

---
+++ EdgesFilterAX.py
@@ -0,0 +1,11 @@
+#pip3 install opencv-python
+#pip3 install matplotlib
+
+import cv2
+import matplotlib.pyplot as plt
+
+im = cv2.imread('input-image.jpg')
+edges = cv2.Canny(im,100,300)
+
+plt.imshow(edges)
+plt.show()
